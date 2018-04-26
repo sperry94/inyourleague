b2036961634c38a6341dafc5aa286bac7c9c0c0a
sperry94 <sepe4375@colorado.edu>
2018-04-26 10:52:46 -0600

    adding service endpoint for team lookup

--- services/event_service/src/event_service.js
+++ services/event_service/src/event_service.js
@@ -1,16 +1,86 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const googleAuth = require('google-auth-library');
+const pg = require('pg');
+
+const googleOAuthClient = new googleAuth.OAuth2Client(process.env.GOOGLE_OAUTH_CLIENT_ID);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const pgPool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+const getGoogleIdFromToken = async (token) => {
+  const oauthInfo = await googleOAuthClient.verifyIdToken({
+    idToken: token,
+    audience: process.env.GOOGLE_OAUTH_CLIENT_ID
+  });
+
+  if(!oauthInfo){
+    console.log('Token verification result was null.');
+    return;
+  }
+
+  const oauthInfoPayload = oauthInfo.getPayload()
+
+  if(!oauthInfoPayload || !oauthInfoPayload.sub) {
+    console.log('Token verification payload or token ID was null.');
+    return;
+  }
+
+  return oauthInfoPayload.sub;
+}
+
+pgPool.connect();
+
+app.get('/team/:teamkey?', async (req, res) => {
+  if(!req.cookies.OAuthToken) {
+    res.status(401).send('No auth token was provided.');
+    return;
+  }
+
+  let userId;
+  try {
+    userId = await getGoogleIdFromToken(req.cookies.OAuthToken);
+  } catch(error) {
+    console.log(error);
+    res.status(401).send('An error occurred when trying to verify the auth token.');
+    return;
+  }
+
+  if(!userId) {
+    res.status(401).send('Token could not be verified.');
+    return;
+  }
+
+  let queryRes;
+  try {
+    queryRes = await pgPool.query('SELECT key, teamkey, name, fullDay, startTime, endTime FROM get_eventlist($1, $2)',
+      [userId, req.params.teamkey]);
+  } catch(error) {
+    console.log(error);
+    res.status(500).send('An error occurred when trying to lookup the event list.');
+    return;
+  }
+
+  if(!queryRes || !queryRes.rows) {
+    console.log('The lookup event list query returned null.');
+    res.status(500).send('An error occurred when trying to lookup the event list.');
+    return;
+  }
+
+  res.status(200).send({
+    eventlist: queryRes.rows
+  });
+});
 
 app.set('port', process.env.PORT || 8888);
 
-app.use(bodyParser.json());
+app.listen(app.get('port'), () => { console.log('Application started.'); });
 
-var count = 0;
-
-app.post('/', (req, res) => {
-  count += 1;
-  res.send({count: count});
-})
-
-app.listen(app.get('port'), () => { console.log('Application started.'); })
+module.exports = app;
