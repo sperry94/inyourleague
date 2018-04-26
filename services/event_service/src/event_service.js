42a10a1941394c4546cbc0a7e33494a77d9a4afd
sperry94 <sepe4375@colorado.edu>
2018-04-26 11:08:03 -0600

    saving events from service

--- services/event_service/src/event_service.js
+++ services/event_service/src/event_service.js
@@ -79,6 +79,85 @@ app.get('/team/:teamkey?', async (req, res) => {
   });
 });
 
+app.put('/', async(req, res) => {
+  if(!req.cookies.OAuthToken) {
+    res.status(401).send('No auth token was provided.');
+    return;
+  }
+
+  let userId;
+  try {
+    userId = await getGoogleIdFromToken(req.cookies.OAuthToken);
+  } catch(error) {
+    console.log(error);
+    res.status(401).send('An error occurred when trying to verify the auth token.');
+    return;
+  }
+
+  if(!userId) {
+    res.status(401).send('Token could not be verified.');
+    return;
+  }
+
+  if(req.body.Name == null || !(typeof(req.body.Name) === "string")) {
+    const msg = 'No event name was specified for the event to save.';
+    console.log(msg);
+    res.status(400).send(msg);
+    return;
+  }
+
+  if(req.body.TeamKey == null || !(typeof(req.body.TeamKey) === "string")) {
+    const msg = 'No team key was specified for the event to save.';
+    console.log(msg);
+    res.status(400).send(msg);
+    return;
+  }
+
+  if(req.body.FullDay == null || !(typeof(req.body.FullDay) === "boolean")) {
+    const msg = 'No event full day status was specified for the event to save.';
+    console.log(msg);
+    res.status(400).send(msg);
+    return;
+  }
+
+  if (req.body.StartTime == null || !(typeof(req.body.StartTime) === "string")) {
+      const msg = 'No event start time was specified for the event to save.';
+      console.log(msg);
+      res.status(400).send(msg);
+      return;
+  }
+
+  if(!req.body.FullDay && (req.body.EndTime == null
+    || !(typeof(req.body.EndTime) === "string"))) {
+      const msg = 'No event end time was specified for the event to save.';
+      console.log(msg);
+      res.status(400).send(msg);
+      return;
+  }
+
+  let queryRes;
+  try {
+    queryRes = await pgPool.query('SELECT save_event($1, $2, $3, $4, $5, $6, $7) as eventkey',
+      [req.body.Key, userId, req.body.TeamKey, req.body.Name,
+        req.body.FullDay, req.body.StartTime, req.body.EndTime]);
+  } catch(error) {
+    console.log(error);
+    res.status(500).send('An error occurred when trying to save the event.');
+    return;
+  }
+
+  if(!queryRes || !queryRes.rows || !queryRes.rows[0]
+    || !queryRes.rows[0].eventkey) {
+    console.log('The save event query returned null.');
+    res.status(500).send('An error occurred when trying to save the event.');
+    return;
+  }
+
+  res.status(200).send({
+    key: queryRes.rows[0].eventkey
+  });
+})
+
 app.set('port', process.env.PORT || 8888);
 
 app.listen(app.get('port'), () => { console.log('Application started.'); });
